5eac8b78587f24a5f8481316559dae515be3f23d
Mukund Solanki <mukundsolanki665@gmail.com>
2023-05-28 00:03:03 +0530

    Added path routes

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var _ = require("lodash");
 
 const app = express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded( {extended:true} ));
-
-
 
 const homeContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 const aboutContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 const contactContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 let formData = [];
+// let readUrl;
 
 app.get("/", function(req, res){
-  res.render("home", { homePage : homeContent , homeObj:formData });
+  // res.render("home", { homePage : homeContent , homeObj:formData , readUrl: readUrl });
+  res.render("home", { homePage : homeContent , homeObj:formData});
 
   // console.log(formData);
 });
@@ -34,12 +35,18 @@ app.get("/compose", function(req,res){
 // Adding the custom route
 
 app.get("/read/:post", function(req,res){
-  let urlTitle = req.params.post;
+  let urlString = _.lowerCase(req.params.post);
+
+  // readUrl = "http://localhost:3000/read/" + urlString;
+  // let urlTitle = _.kebabCase('element.heading');
 
   formData.forEach( function(element){
-    if(urlTitle === element.heading )
+
+    let urlTitle = _.lowerCase(element.heading); 
+    if(urlTitle === urlString )
     {
-      console.log("Fouunded");
+      res.render("post", { postHeading:urlTitle , postDescription:element.description });
+      // console.log("Mathc found");
     }
   })
 })
